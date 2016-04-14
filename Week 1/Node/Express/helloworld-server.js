feaad6ea01d9e8ca6fc2f04b24a3d9f59e689bdc
mecodesta <the3drevolution@gmail.com>
2016-04-14 15:21:43 -0700

    Introduction to Express Part 1

---
+++ Week 1/Node/Express/helloworld-server.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+var express = require('express');
+var http = require('http');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(function(req, res, next) {
+  console.log(req.method+', '+req.url);
+  console.log(req.headers);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<html><body><h1>Hello World from Express</h1></body></html>');
+});
+
+var server = http.createServer(app);
+
+server.listen(port, hostname, function() {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ Week 1/Node/Express/static-server.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+var express = require('express');
+
+//morgan allows us to print out log information on the server side
+var morgan = require('morgan');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname+'/public'));
+
+app.listen(port, hostname, function() {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
